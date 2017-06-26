f788ee66133fc9ff7b56b0ba883c0bbd458a722b
s20a3264 <s20a3264@hotmail.com>
2017-06-26 22:17:50 +0800

    some javascript

--- app/assets/javascripts/manager/settings.js
+++ app/assets/javascripts/manager/settings.js
@@ -1,12 +1,3 @@
-$(document).on('ready page:load', function() {
-	var width = $('#myCarousel').width();
-	var height = width * 0.34;
-	 $('#myCarousel').height(height);
 
-	$( window ).resize(function() {
-		var width = $('#myCarousel').width();
-		var height = width * 0.34;
-		 $('#myCarousel').height(height);
-	});
-	
-});
+
+

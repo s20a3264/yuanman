6198ce5a1721774e36ab1cd43d0a7f5ed971179e
s20a3264 <s20a3264@hotmail.com>
2017-08-18 01:24:45 +0800

    beta1.8

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Estore
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.force_ssl = true
+
   end
 end
